4aec80a6bd705d071698fd5ab9c69041aee23e4e
Michael <michael@MichAI.local>
2026-05-04 14:52:56 +0300

    fix: remove cloudflare adapter for static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://lovemixandmatch.com',
   output: 'static',
-  adapter: cloudflare(),
   build: {
     format: 'directory'
   },
